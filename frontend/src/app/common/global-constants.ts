8b9d19659da3e708ae189fea5dc7a104b9afdc8e
xaniadakis <sdi1700280@di.uoa.gr>
2021-10-09 13:39:07 +0300

    commit changes

--- frontend/src/app/common/global-constants.ts
+++ frontend/src/app/common/global-constants.ts
@@ -1,7 +1,7 @@
 export class GlobalConstants{
-    public static APIURL: string = "http://localhost:3000/"
+    public static APIURL: string = "https://localhost:3000/api/"
     //"https://2549-2a02-2149-8a95-6b00-34a2-2c70-d662-e20f.ngrok.io/api/"
-    public static SOCKETURL: string = "http://localhost:3000/api/"
+    public static SOCKETURL: string = "https://localhost:3000/"
 //    "https://2549-2a02-2149-8a95-6b00-34a2-2c70-d662-e20f.ngrok.io/"
     // "http://localhost:3000/api/"
     // "http://816c-2a02-2149-8a95-6b00-34a2-2c70-d662-e20f.ngrok.io/api/"
